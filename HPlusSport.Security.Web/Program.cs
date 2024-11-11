72ffc5bbeab56b043b95d3888d28e7dc218d99cc
LauraaMolinaa <2232607@johnabbottcollege.net>
2024-11-11 10:23:00 -0500

    test push

--- HPlusSport.Security.Web/Program.cs
+++ HPlusSport.Security.Web/Program.cs
@@ -5,6 +5,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//test
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
