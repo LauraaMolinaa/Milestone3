fa31e0c658c6993fb15ecc86871733dd78c3b2ce
LauraaMolinaa <123187064+LauraaMolinaa@users.noreply.github.com>
2024-11-23 15:28:58 -0500

    halfway thorugh chapter three and dowloaded a few missing packages

--- HPlusSport.Security.identity/Program.cs
+++ HPlusSport.Security.identity/Program.cs
@@ -10,6 +10,9 @@ namespace HPlusSport.Security.identity
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(); 
+
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -26,6 +29,8 @@ namespace HPlusSport.Security.identity
             if (app.Environment.IsDevelopment())
             {
                 app.UseMigrationsEndPoint();
+                app.UseSwagger();
+                app.UseSwaggerUI(); 
             }
             else
             {
@@ -42,6 +47,14 @@ namespace HPlusSport.Security.identity
             app.UseAuthorization();
 
             app.MapRazorPages();
+
+            app.MapSwagger();
+
+            app.MapGet("/api/hello", (HttpContext context) =>
+            {
+                return $"hello {context.User?.Identity?.Name}";
+            })
+                .WithOpenApi(); 
 
             app.Run();
         }
